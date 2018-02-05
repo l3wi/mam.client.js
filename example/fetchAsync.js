a365e8feb5d74c6e4a25b078433dffaead012428
Lewis Freiberg <lewi@2pk.in>
2018-02-05 11:52:25 +0100

    Memory Leaks Fixed (#1)
    * Examples Update
    
    * Prettier formatting
    
    * Drop required ctrits
    
    * Update build files
    
    * Changes: Drop ctrits not input
    
    * Re build libs

--- example/fetchAsync.js
+++ example/fetchAsync.js
@@ -1,6 +1,6 @@
 var Mam = require('../lib/mam.node.js')
 var IOTA = require('iota.lib.js')
-var iota = new IOTA({ provider: `http://p103.iotaledger.net:14700/` })
+var iota = new IOTA({ provider: `https://testnet140.tangle.works` })
 
 // Init State
 let root = ''
@@ -10,24 +10,24 @@ var mamState = Mam.init(iota)
 
 // Publish to tangle
 const publish = async packet => {
-  var trytes = iota.utils.toTrytes(JSON.stringify(packet))
-  var message = Mam.create(mamState, trytes)
-  mamState = message.state
-  await Mam.attach(message.payload, message.address)
-  return message.root
+    var trytes = iota.utils.toTrytes(JSON.stringify(packet))
+    var message = Mam.create(mamState, trytes)
+    mamState = message.state
+    await Mam.attach(message.payload, message.address)
+    return message.root
 }
 
 // Callback used to pass data out of the fetch
 const logData = data => console.log(JSON.parse(iota.utils.fromTrytes(data)))
 
 const execute = async () => {
-  // Publish and save root.
-  root = await publish('POTATOONE')
-  // Publish but not save root
-  await publish('POTATOTWO')
-  // Callback used to pass data + returns next_root
-  var resp = await Mam.fetch(root, 'public', null, logData)
-  console.log(resp)
+    // Publish and save root.
+    root = await publish('POTATOONE')
+    // Publish but not save root
+    await publish('POTATOTWO')
+    // Callback used to pass data + returns next_root
+    var resp = await Mam.fetch(root, 'public', null, logData)
+    console.log(resp)
 }
 
 execute()
