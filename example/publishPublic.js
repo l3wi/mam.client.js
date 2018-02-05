6561060df78004ca783c3ba1bd5526d9b32d9488
Lewis Freiberg <lewi@2pk.in>
2018-02-05 11:06:34 +0100

    Examples Update

--- example/publishPublic.js
+++ example/publishPublic.js
@@ -1,24 +1,24 @@
 var Mam = require('../lib/mam.node.js')
 var IOTA = require('iota.lib.js')
-var iota = new IOTA({ provider: `http://p103.iotaledger.net:14700/` })
+var iota = new IOTA({ provider: `https://testnet140.tangle.works` })
 
 // Initialise MAM State - PUBLIC
 var mamState = Mam.init(iota)
 
 // Publish to tangle
 const publish = async packet => {
-  // Create MAM Payload - STRING OF TRYTES
-  var message = Mam.create(mamState, packet)
-  // Save new mamState
-  mamState = message.state
-  // Attach the payload.
-  console.log('Root: ', message.root)
-  console.log('Address: ', message.address)
-  await Mam.attach(message.payload, message.address)
+    // Create MAM Payload - STRING OF TRYTES
+    var message = Mam.create(mamState, packet)
+    // Save new mamState
+    mamState = message.state
+    // Attach the payload.
+    console.log('Root: ', message.root)
+    console.log('Address: ', message.address)
+    await Mam.attach(message.payload, message.address)
 
-  // Fetch Stream Async to Test
-  var resp = await Mam.fetch(message.root, 'public', null, console.log)
-  console.log(resp)
+    // Fetch Stream Async to Test
+    var resp = await Mam.fetch(message.root, 'public', null, console.log)
+    console.log(resp)
 }
 
 publish('POTATO')
--- example/publishRestricted.js
+++ example/publishRestricted.js
@@ -1,36 +1,36 @@
 var Mam = require('../lib/mam.node.js')
 var IOTA = require('iota.lib.js')
-var iota = new IOTA({ provider: `http://p103.iotaledger.net:14700/` })
+var iota = new IOTA({ provider: `https://testnet140.tangle.works` })
 
 // Initialise MAM State
 var mamState = Mam.init(iota)
 
 // Set channel mode
 mamState = Mam.changeMode(
-  mamState,
-  'restricted',
-  'IREALLYENJOYPOTATORELATEDPRODUCTS'
+    mamState,
+    'restricted',
+    'IREALLYENJOYPOTATORELATEDPRODUCTS'
 )
 
 // Publish to tangle
 const publish = async packet => {
-  // Create MAM Payload - STRING OF TRYTES
-  var message = Mam.create(mamState, packet)
-  // Save new mamState
-  mamState = message.state
-  console.log('Root: ', message.root)
-  console.log('Address: ', message.address)
-  // Attach the payload.
-  await Mam.attach(message.payload, message.address)
+    // Create MAM Payload - STRING OF TRYTES
+    var message = Mam.create(mamState, packet)
+    // Save new mamState
+    mamState = message.state
+    console.log('Root: ', message.root)
+    console.log('Address: ', message.address)
+    // Attach the payload.
+    await Mam.attach(message.payload, message.address)
 
-  // Fetch Stream Async to Test
-  var resp = await Mam.fetch(
-    message.root,
-    'restricted',
-    'IREALLYENJOYPOTATORELATEDPRODUCTS',
-    console.log
-  )
-  console.log(resp)
+    // Fetch Stream Async to Test
+    var resp = await Mam.fetch(
+        message.root,
+        'restricted',
+        'IREALLYENJOYPOTATORELATEDPRODUCTS',
+        console.log
+    )
+    console.log(resp)
 }
 
 publish('POTATO')
