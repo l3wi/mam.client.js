ea50c24a9b0290f0156d131256446639438eb478
Greg Lutostanski <greg.luto@gmail.com>
2018-07-21 22:51:02 +0200

    Do not use the same byte for entire key
    * this is a flaw that limits the keygen to only 27 possible keys.
    
    Hope this helps!

--- src/index.js
+++ src/index.js
@@ -280,7 +280,7 @@ const keyGen = length => {
     const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ9'
     let key = '';
     while (key.length < length) {
-        const byte = crypto.randomBytes(1)
+        let byte = crypto.randomBytes(1)
         if (byte[0] < 243) {
             key += charset.charAt(byte[0] % 27);
         }
