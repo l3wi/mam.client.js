4a207f95bde79e8ddf39f6edc5dd8f559f72da2f
Alexey Sobolev <sobolev@mail.de>
2019-02-05 21:49:36 +0100

    Added new example

---
+++ example/publishAndFetch.js
@@ -0,0 +1,58 @@
+const Mam = require('../lib/mam.client.js')
+const { asciiToTrytes, trytesToAscii } = require('@iota/converter')
+
+const mode = 'restricted'
+const secretKey = 'VERYSECRETKEY'
+const provider = 'https://nodes.devnet.iota.org'
+
+const mamExplorerLink = `https://mam-explorer.firebaseapp.com/?provider=${provider}&mode=${mode}&key=${secretKey.padEnd(81, '9')}&root=`
+
+// Initialise MAM State
+let mamState = Mam.init(provider)
+
+// Set channel mode
+mamState = Mam.changeMode(mamState, mode, secretKey)
+
+// Publish to tangle
+const publish = async packet => {
+    // Create MAM Payload - STRING OF TRYTES
+    const trytes = asciiToTrytes(JSON.stringify(packet))
+    const message = Mam.create(mamState, trytes)
+
+    // Save new mamState
+    mamState = message.state
+
+    // Attach the payload
+    await Mam.attach(message.payload, message.address, 3, 9)
+
+    console.log('Published', packet, '\n');
+    return message.root
+}
+
+const publishAll = async () => {
+  const root = await publish({
+    message: 'Message from Alice',
+    timestamp: (new Date()).toLocaleString()
+  })
+
+  await publish({
+    message: 'Message from Bob',
+    timestamp: (new Date()).toLocaleString()
+  })
+
+  await publish({
+    message: 'Message from Charlie',
+    timestamp: (new Date()).toLocaleString()
+  })
+
+  return root
+}
+
+// Callback used to pass data out of the fetch
+const logData = data => console.log('Fetched and parsed', JSON.parse(trytesToAscii(data)), '\n')
+
+publishAll()
+  .then(async root => {
+    await Mam.fetch(root, mode, secretKey, logData)
+    console.log(`Verify with MAM Explorer:\n${mamExplorerLink}${root}\n`);
+  })
